d7374bafa9639894f265ff0a60f4181bb4c4ef98
USUARIO <USUARIO@LAPTOP-BUKF8MRB>
2023-09-20 23:16:39 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio_05_SMN_5_ACT_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Escribir un programa que permita multiplixar dos numeros enteros sin usar el operador "*"
+
+int num1 = 4;
+int num2 = 2;
+int Resultado = 0;
+
+for (int  i = 0; i < num1; i++)
+{
+    Resultado = Resultado + num2;
+
+}
+Console.WriteLine("Ingrese el numero: " + num1);
+Console.WriteLine("Ingrese el numero: " + num2);
+Console.WriteLine("El producto es: " + Resultado);
+Console.ReadKey();
